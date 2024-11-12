397f0f5062e48f4f756ff1547394c96816109a65
Andriamahay11master <hirimanana@yahoo.fr>
2024-11-12 11:28:13 +0300

    add data

---
+++ src/data/data.ts
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    name: "HTML",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+    link: "https://html.spec.whatwg.org/",
+  },
+  {
+    name: "CSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+    link: "https://www.w3.org/Style/CSS/Overview.en.html",
+  },
+  {
+    name: "JavaScript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+    link: "https://www.javascript.com/",
+  },
+  {
+    name: "React",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
+    link: "https://reactjs.org/",
+  },
+  {
+    name: "Typescript",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "Sass",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1200px-Sass_Logo_Color.svg.png",
+    link: "https://sass-lang.com/",
+  },
+  {
+    name: "Vite",
+    img: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Vitejs.svg",
+    link: "https://vitejs.dev/",
+  },
+];
