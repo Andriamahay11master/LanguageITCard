2ac2aeeafb8cb8a93bf282a297ec5321ebbdf3b4
Andriamahay11master <hirimanana@yahoo.fr>
2024-11-12 11:36:33 +0300

    add main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.scss";
+import Card from "./components/Card";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Card />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+import Language from "./Language";
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Language</h1>
+      <div>
+        <Language />
+      </div>
+    </div>
+  );
+};
+export default Card;
