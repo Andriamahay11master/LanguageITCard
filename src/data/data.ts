8ca4262577324b15e47cc600bc4e5bdf17c76b3f
Andriamahay11master <hirimanana@yahoo.fr>
2024-11-14 11:55:55 +0300

    add new data

--- src/data/data.ts
+++ src/data/data.ts
@@ -10,14 +10,14 @@ export const data = [
     link: "https://www.w3.org/Style/CSS/Overview.en.html",
   },
   {
+    name: "Sass",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1200px-Sass_Logo_Color.svg.png",
+    link: "https://sass-lang.com/",
+  },
+  {
     name: "JavaScript",
     img: "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
     link: "https://www.javascript.com/",
-  },
-  {
-    name: "React",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    link: "https://reactjs.org/",
   },
   {
     name: "Typescript",
@@ -25,14 +25,69 @@ export const data = [
     link: "https://www.typescriptlang.org/",
   },
   {
-    name: "Sass",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1200px-Sass_Logo_Color.svg.png",
-    link: "https://sass-lang.com/",
+    name: "JQuery",
+    img: "https://miro.medium.com/v2/resize:fit:860/0*eFomJUFua8tuqe8g.png",
+    link: "https://jquery.com/",
+  },
+  {
+    name: "React",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
+    link: "https://reactjs.org/",
+  },
+  {
+    name: "Next.js",
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg",
+    link: "https://nextjs.org/",
+  },
+  {
+    name: "Bootstrap",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png",
+    link: "https://getbootstrap.com/",
+  },
+  {
+    name: "Tailwind",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+    link: "https://tailwindcss.com/",
+  },
+  {
+    name: "Python",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
+    link: "https://www.python.org/",
   },
   {
     name: "Vite",
     img: "https://vitejs.fr/logo-with-shadow.png",
     link: "https://vitejs.dev/",
+  },
+  {
+    name: "Git",
+    img: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+    link: "https://git-scm.com/",
+  },
+  {
+    name: "Firebase",
+    img: "https://images.ctfassets.net/in6v9lxmm5c8/57MXX73Nx2rUnUI730EykA/8d13236943bb46948155f92f2325369e/firebase.png?q=50&fm=webp",
+    link: "https://firebase.google.com/",
+  },
+  {
+    name: "MySQL",
+    img: "https://images.javatpoint.com/mysql/images/mysql-tutorial.png",
+    link: "https://www.mysql.com/",
+  },
+  {
+    name: "PostgreSQL",
+    img: "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+    link: "https://www.postgresql.org/",
+  },
+  {
+    name: "Figma",
+    img: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+    link: "https://www.figma.com/",
+  },
+  {
+    name: "Office 365",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Microsoft_365_%282022%29.svg/1862px-Microsoft_365_%282022%29.svg.png",
+    link: "https://www.microsoft.com/en-us/microsoft-365",
   },
 ];
 export default data;
