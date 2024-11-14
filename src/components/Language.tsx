67189fb503c33a048770086243afb75455942124
Andriamahay11master <hirimanana@yahoo.fr>
2024-11-14 11:26:45 +0300

    add state show + animation

--- src/components/Language.tsx
+++ src/components/Language.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 interface LanguageProps {
   name: string;
   img: string;
   link: string;
 }
 const Language = ({ name, img, link }: LanguageProps) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="cardWrapper">
-      <img src={img} alt="lang" />
-      <h3>{name}</h3>
-      <a href={link}>See the website</a>
+    <div className="cardWrapper" onClick={() => setShow(!show)}>
+      <div className={!show ? "cardTop show" : "cardTop"}>
+        <img src={img} alt="lang" />
+        <h3>{name}</h3>
+      </div>
+      <div className={show ? "cardBottom show" : "cardBottom"}>
+        <a href={link}>See the website</a>
+      </div>
     </div>
   );
 };
